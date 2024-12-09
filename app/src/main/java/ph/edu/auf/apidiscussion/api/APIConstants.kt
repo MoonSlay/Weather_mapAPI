c04253ad8c00b908f983b05b135b35a2d7384062
Angelo <delrosarioangelo18@gmail.com>
2024-12-09 17:06:59 +0800

    Initial commit

---
+++ app/src/main/java/ph/edu/auf/apidiscussion/api/APIConstants.kt
@@ -0,0 +1,7 @@
+package ph.edu.auf.apidiscussion.api
+
+object APIConstants {
+    const val WEATHER_API_BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    const val WEATHER_API_KEY = "946c93624193ab5ba673caca531c9894"
+    const val WEATHER_ICON_BASE_URL = "https://openweathermap.org/img/wn/"
+}
